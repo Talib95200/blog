5c9474194f763af40962a7c28404b2f74e9e6a40
Talib95200 <talibsaifi99340@gmail.com>
2026-04-28 13:45:18 +0530

    Change URl

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,13 +1,12 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import toast from "react-hot-toast";
-
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = "https://blog-cysk.onrender.com";
 
 const AppContext = createContext();
 
-export const AppProvider = ({ children })=> {
+export const AppProvider = ({ children }) => {
 
     const navigate = useNavigate()
 
@@ -15,28 +14,28 @@ export const AppProvider = ({ children })=> {
     const [blogs, setBlogs] = useState([])
     const [input, setInput] = useState("")
 
-    const fetchBlogs = async ()=>{
+    const fetchBlogs = async () => {
         try {
-            const {data} = await axios.get('/api/blog/all');
+            const { data } = await axios.get('/api/blog/all');
             data.success ? setBlogs(data.blogs) : toast.error(data.message)
         } catch (error) {
             toast.error(error.message)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchBlogs();
         const token = localStorage.getItem('token')
-        if(token){
+        if (token) {
             setToken(token);
             axios.defaults.headers.common['Authorization'] = `${token}`;
         }
-    },[])
+    }, [])
 
     const value = {
         axios, navigate, token, setToken, blogs, setBlogs, input, setInput
     }
-    
+
     return (
         <AppContext.Provider value={value}>
             {children}
@@ -45,6 +44,6 @@ export const AppProvider = ({ children })=> {
 
 }
 
-export const useAppContext = ()=> {
+export const useAppContext = () => {
     return useContext(AppContext)
 }
